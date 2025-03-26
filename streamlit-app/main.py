98c9ae4eab3b5a8324fac7327c67d3dfea1a8e42
Folk N <81551496+folkadonis@users.noreply.github.com>
2025-03-26 20:50:30 +0530

    Create main.py

---
+++ streamlit-app/main.py
@@ -0,0 +1,41 @@
+import streamlit as st
+from langchain_ollama import ChatOllama
+from langchain_core.messages import HumanMessage, SystemMessage, AIMessage
+import os
+
+OLLAMA_API_URL = os.getenv("OLLAMA_API_URL", "http://localhost:11434")
+
+st.title("Chatbot 🎬")
+
+if "messages" not in st.session_state:
+    st.session_state.messages = [
+        SystemMessage(content="Act like Jarvis in Iron Man movie")
+    ]
+
+for message in st.session_state.messages:
+    if isinstance(message, HumanMessage):
+        with st.chat_message("user"):
+            st.markdown(message.content)
+    elif isinstance(message, AIMessage):
+        with st.chat_message("assistant"):
+            st.markdown(message.content)
+
+prompt = st.chat_input("Type your message...")
+
+if prompt:
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    st.session_state.messages.append(HumanMessage(content=prompt))
+
+    llm = ChatOllama(
+        model="llama3.2:latest",
+        temperature=0.7,
+        base_url=OLLAMA_API_URL
+    )
+
+    result = llm.invoke(st.session_state.messages).content
+
+    with st.chat_message("assistant"):
+        st.markdown(result)
+
+    st.session_state.messages.append(AIMessage(content=result))
